b8238e8c879a696c0e16d93738c52928f6da000c
Diego Cando <diegoramoncito@gmail.com>
2022-03-24 10:16:10 -0500

    error de limpieza

--- web/tools/toConn.php
+++ web/tools/toConn.php
@@ -159,7 +159,7 @@ function valNumber($in){
 function clean($string) {
    $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
 
-   return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+   return preg_replace('/[^A-Za-z\-]/', '', $string); // Removes special chars.
 }
 
 ?>
