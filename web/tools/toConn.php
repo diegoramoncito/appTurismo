381dd2dd3eaf25b4967b4e8399bca97fc762cc84
Diego Cando <diegoramoncito@gmail.com>
2022-03-24 10:21:04 -0500

    correction de limpieza

--- web/tools/toConn.php
+++ web/tools/toConn.php
@@ -157,9 +157,9 @@ function valNumber($in){
 }
 
 function clean($string) {
-   $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
+   $string = str_replace(' ', '', $string); // Replaces all spaces with hyphens.
 
-   return preg_replace('/[^A-Za-z\-]/', '', $string); // Removes special chars.
+   return preg_replace('/[^0-9]/', '', $string); // Removes special chars.
 }
 
 ?>
